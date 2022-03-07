3d558589b93967204b8f5ad501f3d707113a417e
Anton Ekblad <anton@ekblad.cc>
2022-03-07 17:13:59 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
     // If you're importing a library and not a plugin, just move this whole section to build.gradle.kts.
     repositories {
         maven {
-            url = uri("https://maven.pkg.github.com/valderman/gradle-plugin-on-github-packages")
+            url = uri("https://maven.pkg.github.com/valderman/gradle-plugin-on-github-packages-maven-repo")
             credentials {
                 username = System.getenv("GITHUB_USERNAME")
                 password = System.getenv("GITHUB_TOKEN")
